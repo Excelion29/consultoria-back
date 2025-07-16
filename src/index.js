fd24a77f69795fa493b8510b987946201e669e77
Excelion29 <wronkowcanolt@gmail.com>
2025-07-16 13:05:10 -0500

    refactor: gitignore

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import 'dotenv/config.js';
 import express from 'express';
 import cors from 'cors';
-import swaggerUi from 'swagger-ui-express';
 import authRoutes from './routes/auth.route.js';
-import swaggerSpec from './swagger.js'; 
+import swaggerUi from 'swagger-ui-express';
+import swaggerSpec from './swagger.js';
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
